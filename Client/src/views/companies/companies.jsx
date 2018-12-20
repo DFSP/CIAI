2ce9799f8f40235aad386b105d85a8cbf71da369
Daniel Pimenta <d.pimenta@campus.fct.unl.pt>
2018-12-20 16:34:33 +0000

    Sidebar + appbar + footer + router

---
+++ Client/src/views/companies/companies.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const companies = () => (
+    <div>
+
+    </div>
+);
+
+export default companies
--- Client/src/views/ecmaEvents.test.tsx
+++ Client/src/views/ecmaEvents.test.tsx
@@ -1,9 +1,9 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import App from './App';
+import EcmaEvents from './ecmaEvents';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  ReactDOM.render(<EcmaEvents />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
